e4240560c1b2ae5a629467b8bbd76c2ec5f214c4
mikespandrio <michael.spandrio@gmail.com>
2019-10-26 11:10:27 -0700

    First commit

---
+++ chores-app/src/test/java/com/teamtrouble/web/choresapplication/ChoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamtrouble.web.choresapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
